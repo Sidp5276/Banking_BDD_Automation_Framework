fb1d27305aaae73e1d324294ddf6677695fb8e52
Siddharth <sidp5276@gmail.com>
2026-06-21 16:51:42 +0530

    init commit

---
+++ src/main/java/com/ecommerce/framework/utils/ScreenshotUtil.java
@@ -0,0 +1,33 @@
+package com.ecommerce.framework.utils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Utility to capture screenshots on failure.
+ */
+public class ScreenshotUtil {
+    public static String takeScreenshot(WebDriver driver, String name) {
+        try {
+            if (!(driver instanceof TakesScreenshot)) return null;
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String ts = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+            Path target = Path.of("target", "screenshots", name + "_" + ts + ".png");
+            Files.createDirectories(target.getParent());
+            Files.copy(src.toPath(), target, StandardCopyOption.REPLACE_EXISTING);
+            return target.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/test/java/com/ecommerce/framework/runners/TestRunner.java
@@ -0,0 +1,21 @@
+package com.ecommerce.framework.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {
+                "com.ecommerce.framework.stepdefinitions",
+                "com.ecommerce.framework.hooks"
+        },
+        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"}
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
